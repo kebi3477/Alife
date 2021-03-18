e6906a729f5c55104b3bafda52d1ad830eaa665a
kebi3477 <kebi3477@naver.com>
2021-03-18 22:20:21 +0900

    눈 깜빡임 추가

--- js/index.js
+++ js/index.js
@@ -1,17 +1,31 @@
 const slides = document.querySelectorAll(".slide");
+const eyeBox = document.querySelector(".eye-box");
 const eye = document.querySelector(".eye");
 const eyeIn = eye.querySelector(".eye-in");
 let scrolling = true;
 let preScroll = 0;
 let now = 0;
-
+let x=0, y=0, reverseX=false, reverseY=false;
+setInterval(function() {
+    eyeBox.style.left = `${x}px`;
+    if(eyeBox.offsetLeft + eyeBox.offsetWidth > window.innerWidth) {
+        reverseX = true;
+    } else if(eyeBox.offsetLeft <= 0) {
+        reverseX = false;
+    }
+    if(reverseX) {
+        x-=2;
+    } else {
+        x+=2; 
+    }
+} ,1);
 // window.scroll(0, 0);
 window.onmousemove = e => {
-    const innerWidth = window.innerWidth/2;
-    const innerHeight = window.innerHeight/2;
+    const boxPositionX = eyeBox.offsetLeft+eyeBox.offsetWidth/2;
+    const boxPositionY = eyeBox.offsetTop+eyeBox.offsetHeight/2;
     const range = 30;
-    const left = (e.clientX-innerWidth)/range;
-    const top = (e.clientY-innerHeight)/range;
+    const left = (e.clientX-boxPositionX)/range;
+    const top = (e.clientY-boxPositionY)/range;
     eyeIn.style.transform = `translate(${left}px, ${top}px)`;
 }
 window.onscroll = () => {
