3f6c19f60aa3d055d91a2af78bd32c66f9234806
kebi3477 <kebi3477@naver.com>
2021-03-26 17:44:37 +0900

    Add Fonts and header

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alife</title>
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header>
+        <div class="header__list">
+            <img class="header__logo" src="" alt="">
+            <div class="header__item">나의 냉장고</div>
+            <div class="header__item">레시피</div>
+            <div class="header__item">밀키트</div>
+            <div class="header__item">오늘 뭐 먹지?</div>
+            <div class="header__item header__item--gray">더 보기</div>
+        </div>
+        <div class="header__list">
+            <div class="header__item header__item--small">로그인</div>
+            <div class="header__item header__item--small">회원가입</div>
+        </div>
+    </header>
+</body>
+</html>
