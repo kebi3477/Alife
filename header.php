cbfda9e7ab2e3321c09d102766d67792c8741962
kebi3477 <kebi3477@naver.com>
2021-03-30 22:27:44 +0900

    메뉴 이미지 크기 및 로그인 이미지 비율 수정

--- header.php
+++ header.php
@@ -11,21 +11,7 @@
     <div class="header__list">
         <div class="header__item header__item--small"><a href="login.php">로그인</a></div>
         <div class="header__item header__item--small">회원가입</div>
-        <svg class="header__item header__search" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
-            <g id="그룹_74" transform="translate(978.671 -520.248)">
-                <path id="패스_39" d="M-945.406,553.179l-7.648-7.648a11.334,11.334,0,0,0,3.275-7.973,11.388,11.388,0,0,0-11.375-11.376,11.388,11.388,0,0,0-11.375,11.376,11.388,11.388,0,0,0,11.375,11.375,11.307,11.307,0,0,0,6.586-2.118l7.762,7.762Zm-25.144-15.621a9.407,9.407,0,0,1,9.4-9.4,9.408,9.408,0,0,1,9.4,9.4,9.407,9.407,0,0,1-9.4,9.4A9.406,9.406,0,0,1-970.55,537.559Z" transform="translate(-0.067 -0.06)" fill="#1a1818"/>
-            </g>
-        </svg>
-        <svg class="header__item header__menu" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
-            <g id="그룹_182" transform="translate(-1830 -614.79)">
-                <g id="그룹_76" transform="translate(1830 615)">
-                <g id="그룹_75" transform="translate(6.705 9.278)">
-                    <rect id="사각형_48" width="26.38" height="1.989" fill="#1a1818"/>
-                    <rect id="사각형_49" width="26.38" height="1.989" transform="translate(0 9.622)" fill="#1a1818"/>
-                    <rect id="사각형_50" width="26.38" height="1.989" transform="translate(0 19.244)" fill="#1a1818"/>
-                </g>
-                </g>
-            </g>
-        </svg>
+        <object class="header__img" data="images/icon/search.svg" type="image/svg+xml"></object>
+        <object class="header__img" data="images/icon/menu.svg" type="image/svg+xml"></object>
     </div>
 </header>
