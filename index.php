073e44072c6d326cfb51ae16cea72014d545f5ea
kebi3477 <kebi3477@naver.com>
2021-04-06 21:41:18 +0900

    Add 404 page

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
     $urls = explode("/",$_SERVER['REQUEST_URI']);
-    $type = $urls[2];
+    if($urls[1] == "") {
+        $type = 'index';
+    } else {
+        $type = $urls[1];
+    }
     if($type == 'controller') {
         $dir = '\\controller\\';
     } else if($type == 'model') {
         $dir = '\\model\\';
     } else {
         $dir = '\\public\\views\\';
-        require_once __DIR__.$dir.$type.".php";
+        $url = __DIR__.$dir.$type.".php";
+        if(is_file($url)) {
+            require_once $url;
+        } else {
+            require_once __DIR__.'\\public\\error\\404.php';
+        }
     }
 ?>
