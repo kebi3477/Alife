ed0d36ca891ac01438ec4d7fae1a588dcb94effe
kebi3477 <kebi3477@naver.com>
2021-04-23 00:42:11 +0900

    Init insert.php

---
+++ public/views/insert.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALife</title>
+    <link rel="stylesheet" href="public/css/common.css">
+    <link rel="stylesheet" href="public/css/insert.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+    <div class="insert">
+        <div class="insert__title">레시피 등록</div>
+    </div>
+</body>
+</html>
