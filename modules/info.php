46ac7954783523c951b79f158608851446f17674
kebi3477 <kebi3477@naver.com>
2021-08-31 14:27:07 +0900

    Delete info.php

--- modules/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function getInfo() {
-        $obj = new ArrayObject();
-        $obj->username = "kebi6270@gmail.com";
-        $obj->password = "ehdals1204";
-        return $obj;
-    }
-?>
